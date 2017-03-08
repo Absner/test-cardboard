48bcc9480878041095e06d11672200666e8c51ac
Absner <absrraniel@gmail.com>
2017-03-08 03:28:14 -0400

    Completando el dashboard,  ademas de realizar el modelo relacional de la BD, crear los formulario para add videos

---
+++ js/main.js
@@ -0,0 +1,10 @@
+/*$('.button-collapse').sideNav({
+      menuWidth: 300, // Default is 240
+      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    }
+  );
+  $('.collapsible').collapsible();*/
+  $(document).ready(function(){
+    $('ul.tabs').tabs();
+    $('select').material_select();
+  });
